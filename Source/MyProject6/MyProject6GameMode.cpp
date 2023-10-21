57c46e6e67cf9d22713111936a45616656c155dc
Felixs123 <101900450+Felixs123@users.noreply.github.com>
2023-10-21 18:17:28 -0400

    first commit

---
+++ Source/MyProject6/MyProject6GameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyProject6GameMode.h"
+#include "MyProject6Character.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMyProject6GameMode::AMyProject6GameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
